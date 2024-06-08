d75aae49b890cfabac2be9999a41444b9330be38
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-06-09 02:19:43 +0500

    refactor: undecided section

--- components/hadith/index.tsx
+++ components/hadith/index.tsx
@@ -4,6 +4,8 @@ import { motion, useScroll, useTransform, useMotionValue } from "framer-motion";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import KABAH from "@/assets/KABAH.jpg";
+import Image from "next/image";
 
 const EnabledDot = () => (
   <svg
@@ -66,6 +68,31 @@ const Hadith = () => {
 
   return (
     <div className="text-black">
+      <div className="mx-24 mb-20 flex items-center justify-between">
+        <div className="flex flex-col gap-6 pr-20">
+          <h1 className="readex mt-0 text-5xl font-semibold leading-[120%] text-white">
+            I provide artistic <span className="text-[#FB5322]">solutions</span>{" "}
+            to meet your business requirements.
+          </h1>
+          <span className="readex text-xl font-[300] text-[#a4a4a4]">
+            Direct your energy towards growing your business, and entrust me
+            with the task of efficiently presenting your business in the digital
+            world, setting it apart from competitors.
+          </span>
+        </div>
+        <div className="relative h-[649px] min-w-[449px] overflow-hidden rounded-xl">
+          {/* "https://i.pinimg.com/originals/f2/35/01/f235016367d4708d2b1e851a338dc51b.jpg" */}
+          {/* "https://i.pinimg.com/originals/c6/42/9c/c6429ca185919142ff176c3be02ea4f1.jpg" */}
+          <Image
+            src={
+              "https://i.pinimg.com/originals/b3/e1/46/b3e1465360a979b69676242154ed86d3.jpg"
+            }
+            alt="My Image"
+            fill
+          />
+        </div>
+      </div>
+
       <div className="readex about-text abc ml-24 mt-7">
         <h1 className="text-outline">Hadiths</h1>
       </div>

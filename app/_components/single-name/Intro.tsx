565e18bb2611ab48a2945482fbed0502c419c477
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-06-28 02:04:32 +0500

    feat: :art: array instead of string

--- app/_components/single-name/Intro.tsx
+++ app/_components/single-name/Intro.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Intro = ({ intro }: { intro: string }) => {
+const Intro = ({ intro }: { intro: string[] }) => {
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: intro }}
-      className="outfit mt-4 w-[70%] text-justify text-lg leading-[28px] text-grey"
-    ></div>
+    <div className="flex w-[70%] flex-col gap-2">
+      {intro.map((v) => {
+        return (
+          <div className="readex mt-4 text-justify text-lg text-grey">{v}</div>
+        );
+      })}
+    </div>
   );
 };
 

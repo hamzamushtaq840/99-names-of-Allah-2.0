6c84e6098283487620a4cf8946e54807640a6963
Hamza Mushtaq <hamzamushtaq840@gmail.com>
2024-10-11 16:02:11 +0500

    feat(sitemap): add dynamic sitemap generation

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.99namesofallah.faith/",
+      lastModified: "2024-10-11T05:00:27+00:00",
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.99namesofallah.faith/name/AR-RAHMAAN",
+      lastModified: "2024-10-11T05:00:27+00:00",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.99namesofallah.faith/name/AR-RAHEEM",
+      lastModified: "2024-10-11T05:00:27+00:00",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.99namesofallah.faith/name/AL-MALIK",
+      lastModified: "2024-10-11T05:00:27+00:00",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.99namesofallah.faith/name/AL-QUDDUS",
+      lastModified: "2024-10-11T05:00:27+00:00",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}

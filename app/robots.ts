fcf26ef18a46d849155e0358de7c684fcc093945
Hamza Mushtaq <hamzamushtaq840@gmail.com>
2024-10-13 22:46:39 +0500

    fix(robots): remove old and add dynamic

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://www.99namesofallah.faith/sitemap.xml",
+  };
+}

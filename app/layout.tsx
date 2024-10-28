27bea1fb844fbe646b8cdad28f1b2b6b4cbcae8c
Hamza Mushtaq <hamzamushtaq840@gmail.com>
2024-10-28 15:42:25 +0500

    feat(og): test og

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,11 @@ import "./global.css";
 export const metadata = {
   title: "99 Names of Allah - Asma Ul Husna Explained with Meanings",
   description: `99 Names of Allah (SWT) with English meanings, descriptions, along with the Arabic pronunciation for each name.`,
+  openGraph: {
+    openGraphImage: "https://www.99namesofallah.faith/unnamed.jpg",
+    title: "99 Names of Allah - Asma Ul Husna Explained with Meanings",
+    description: "Acme is a...",
+  },
 };
 
 export default function RootLayout({
